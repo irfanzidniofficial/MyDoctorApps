70916dd202bf0c3bbd923138bdbc713b7b06f1eb
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-08-08 07:34:47 +0700

    restrucure project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ILLogo} from './assets';
+
+const App = () => {
+  return (
+    <View>
+      <ILLogo />
+      <Text>Welcome App JS</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
